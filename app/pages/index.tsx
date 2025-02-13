cbb37b1662e0f01a701298b747cfea0773497fd5
unknown <o.saini@iitg.ac.in>
2025-02-13 20:55:38 +0530

    base

---
+++ app/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Define a functional component for the Home page
+const Home: React.FC = () => {
+
+  return (
+    <div>
+      <h1>Welcome to Presenting app</h1>
+      <p>This is the home page!</p>
+      <button>click me</button>
+    </div>
+  );
+};
+
+export default Home;
